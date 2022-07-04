6b01de39c143bfd0e7f953b3897d3f532f709dfe
Mphomor <105482283+Mphomor@users.noreply.github.com>
2022-07-04 11:20:58 +0200

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,24 @@
+const data = {
+  products: [
+    {
+      id: '1',
+      name: 'MacBook',
+      price: 8400,
+      image: 'https://picsum.photos/id/180/2400/1600',
+    },
+    {
+      id: '2',
+      name: 'HP',
+      price: 6400,
+      image: 'https://i.pcmag.com/imagery/reviews/042gs1P3GD5s7BjaZpL5YMm-1..v1609799328.jpg',
+    },
+    {
+      id: '3',
+      name: 'iphone 11',
+      price: 6000,
+      image: 'https://cdn.arstechnica.net/wp-content/uploads/2019/09/iPhone-11-Pro-and-11-Pro-Max-back-1-640x467.jpg',
+
+    }
+  ],
+};
+export default data;
